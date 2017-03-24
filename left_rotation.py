04561e988496837f225d5c3dc98db241ac23f493
agerista <amandacrawfordcodes@gmail.com>
2017-03-24 21:43:29 +0000

    optomized left_rotation

--- left_rotation.py
+++ left_rotation.py
@@ -5,6 +5,8 @@ def array_left_rotation(n, k, a):
 
     >>> array_left_rotation(5, 4, [1, 2, 3, 4, 5])
     5 1 2 3 4
+
+    un-optomized solution, terrible answer: run-time = O(n*k)
     """
 
     while k > 0:
@@ -13,7 +15,23 @@ def array_left_rotation(n, k, a):
         a.append(rotation)
         k -= 1
 
-    print a
+    print ' '.join(str(x) for x in a)
+
+
+def arr_left_rot(n, k, a):
+    """Given an array of n integers and a number, d, perform d left rotations on
+    the array. Then print the updated array as a single line of space-separated
+    integers.
+
+    >>> array_left_rotation(5, 4, [1, 2, 3, 4, 5])
+    5 1 2 3 4
+
+    more optomized solution, run-time = O(1)
+
+    """
+
+    answer = a[k:] + a[:k]
+    print ' '.join(str(x) for x in answer)
 
 ################################################################################
 if __name__ == "__main__":
