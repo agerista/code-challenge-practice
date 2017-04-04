42879aca8712bc0be38fd865a1a1ef915952c7e0
agerista <amandacrawfordcodes@gmail.com>
2017-04-04 00:29:08 +0000

    starting palindrome checker

---
+++ got.py
@@ -0,0 +1,30 @@
+def possible_palindromes(astr):
+    """ game of thrones 1 -- any possible palindrome --
+    Write the code to find the required palindrome and then assign the variable
+    'found' a value of True or False
+
+    test 1
+    aaabbbb
+    Yes
+    bbaaabb
+
+    test 2
+    cdefghmnopqrstuvw
+    No
+
+    test 3
+    cdcdcdcdeeeef
+    cdcdeefeedcdc2
+
+    """
+
+    
+
+
+
+    found = False
+
+    if not found:
+        print("NO")
+    else:
+        print("YES")
