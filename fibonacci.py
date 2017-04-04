6fbc625942f100d08f497fff14ba8e05b9ccbe0d
agerista <amandacrawfordcodes@gmail.com>
2017-04-04 02:03:22 +0000

    fib non-recursive fixed

--- fibonacci.py
+++ fibonacci.py
@@ -12,15 +12,43 @@ def fibonacci(n):
 
     x = 0
     y = 1
-    fib = 0
+    i = 2
 
-    while fib <= n:
+    while i <= n:
 
-        fib = x + y
-        x = y
-        y = fib
+        if n == 0:
+            return 0
+
+        elif n == 1:
+            return 1
+
+        else:
+
+            fib = x + y
+            x = y
+            y = fib
+
+        i += 1
+
 
     return fib
+
+# def fibonacci_recursive(n1=0, n2=1):
+#     """add numbers in a fibonacci sequence recursively
+
+#     fibonacci(0) = 0
+#     fibonacci(1) = 1
+#     fibonacci(2) = (0+1) = 1
+#     fibonacci(3) = (1+1) = 2
+#     fibonacci(4) = (1+2) = 3
+#     fibonacci(5) = (2+3) = 5
+#     fibonacci(6) = (3+5) = 8
+#     """
+
+#     if n == 0:
+#         return 0
+
+#     return fib = fibonacci_recursive(n1, n2)
 
 ################################################################################
 if __name__ == "__main__":
