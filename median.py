9feca65fdd1e870d8c0e500542ca0a100660e730
agerista <amandacrawfordcodes@gmail.com>
2017-04-11 21:46:44 +0000

    find the median

---
+++ median.py
@@ -0,0 +1,29 @@
+def calculate_median_rating(businesses):
+
+    new_list = []
+    half = len(new_list) / 2
+
+    for business in businesses:
+
+        new_list.append(business[1])
+
+    if len(new_list) < 1:
+
+        return None
+
+    elif len(new_list) % 2:
+
+        return len(new_list - 1) / 2.0
+
+    else:
+
+        return len(new_list - 1) + len(new_list) / 2
+
+################################################################################
+if __name__ == "__main__":
+    import doctest
+
+    result = doctest.testmod()
+    if not result.failed:
+        print "ALL TESTS PASSED. GOOD WORK!"
+    print
