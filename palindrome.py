3835d6970096860fd9c4a022bbd4b43a5be18565
agerista <amandacrawfordcodes@gmail.com>
2017-04-17 18:59:48 +0000

    palindrome checker solution 2, non-recursive

--- palindrome.py
+++ palindrome.py
@@ -28,9 +28,42 @@ def validator(word):
     word = word.lower()
 
     if not word.isalnum():
-        word = ''.join(c for c in word if c.isalnum())
+        word = ''.join(character for character in word if character.isalnum())
 
     return word
+
+
+def is_palindrome2(word):
+    """If a given string is the spelled the same forwards and backwards, it is a
+    palindrome. You must remove all non-alphanumeric characters.
+
+    >>> is_palindrome("RaceCar")
+    True
+
+    >>> is_palindrome("2a3*3a2")
+    True
+
+    >>> is_palindrome("Tattoo")
+    False
+    """
+
+    word = word.lower()
+
+    if not word.isalnum():
+        word = ''.join(character for character in word if character.isalnum())
+
+    if word == "":
+        return True
+
+    while len(word) > 1:
+
+        if word[0] == word[-1]:
+            word = word[1:-1]
+
+        else:
+            return False
+
+    return True
 
 ################################################################################
 if __name__ == "__main__":
