ff49e4fa7cbd32cd611a26611703d45d6c15172c
agerista <amandacrawfordcodes@gmail.com>
2017-07-21 23:26:17 +0000

    another solution

--- other_code_challenges/count_recursive.py
+++ other_code_challenges/count_recursive.py
@@ -12,6 +12,22 @@ def count_recursively(lst):
 
     return 1 + count_recursively(lst[1:])
 
+
+def count_recursively2(lst, count=0):
+    """Return number of items in a list, using recursion.
+
+    >>> count_recursively2([])
+    0
+
+    >>> count_recursively2([1, 2, 3])
+    3
+    """
+
+    if lst == []:
+        return count
+
+    return count_recursively2(lst[1:], count + 1)
+
 ################################################################################
 if __name__ == "__main__":
     import doctest
